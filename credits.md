98492bf2dcc138aff6f1697d44cd1f8672c867e9
EMCain <11471197+EMCain@users.noreply.github.com>
2026-05-16 17:42:27 -0700

    add colliders to tilemap

--- credits.md
+++ credits.md
@@ -3,5 +3,15 @@ Images:
 background
 https://byandrox.itch.io/industrial-environment-pixel-art-assets
 
-code:
+sprites:
+https://cyberrumor.itch.io/animated-robed-character
+
+code libraries:
 https://github.com/kikito/anim8
+github.com/vrld/hump
+github.com/a327ex/windfield/
+
+I used Tiled to create tilemaps
+
+tutorials
+https://www.youtube.com/watch?v=F3zKl70RJlk
--- main.lua
+++ main.lua
@@ -2,32 +2,45 @@ if arg[2] == "debug" then
     require("lldebugger").start()
 end
 
-local wizard 
+-- imports
 local gameMap
+local cam1
 
-local squares
+local wizard
+local squares -- pixels in one "square" on the game grid
+local world
+local walls
 
 function love.load()
-    sti = require('lib/sti')
-    gameMap = sti('maps/basicFloor.lua')
+    local sti = require('lib/sti')
+    gameMap = sti('maps/floor example.lua')
 
-    camera = require('lib/camera')
-    cam1 = camera()
+    local camera = require('lib/camera')
+    cam1 = camera.new(nil, nil, 2)
 
-    anim8 = require('lib/anim8')
+    local anim8 = require('lib/anim8')
+    local wf = require('lib/windfield')
+
+    world = wf.newWorld(0, 0)
+
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     squares = 16
 
     wizard = {}
+
     wizard.x = 5 * squares
     wizard.y = 3 * squares
-    wizard.speed = 48
+    wizard.speed = 6 * squares
     wizard.scale = 2
     wizard.spriteSheet = love.graphics.newImage('sprites/player/wizard.png')
+
+    wizard.width = 16
+    wizard.height = 32
+
     wizard.grid = anim8.newGrid(
-        16,
-        32,
+        wizard.width,
+        wizard.height,
         wizard.spriteSheet:getWidth(),
         wizard.spriteSheet:getHeight()
     )
@@ -39,6 +52,31 @@ function love.load()
     wizard.animation.right = anim8.newAnimation(wizard.grid('13-16', 1), 0.15)
 
     wizard.currentAnimation = wizard.animation.up
+
+    wizard.collider = world:newBSGRectangleCollider(
+        100,
+        250,
+        wizard.width * 2,
+        wizard.height, -- would use wizard.height if we wanted it to be the full height; instead, its hat goes above the collider a bit
+        wizard.width * 0.5
+    )
+    wizard.collider:setFixedRotation(true)
+
+    print('loading')
+    if (gameMap.layers['obstacles']) then
+        walls = {}
+        for i, obj in pairs(gameMap.layers['obstacles'].objects) do
+            if obj.width > 0 and obj.height > 0 then
+                local wall = world:newRectangleCollider(obj.x, obj.y, obj.width, obj.height)
+                wall:setType('static')
+                table.insert(walls, wall)
+            end
+        end
+    else 
+        debug.debug()
+    end
+
+
 
 end
 
@@ -68,21 +106,43 @@ function love.update(dt)
     if vectorX == 0 and vectorY == 0 then
         wizard.currentAnimation:gotoFrame(2)
     -- normalize speed
-    elseif vectorX ~= 0 and vectorY ~=0 then 
+    elseif vectorX ~= 0 and vectorY ~=0 then
         vectorX = vectorX * .707
         vectorY = vectorY * .707
     end
 
-    wizard.x = wizard.x + (vectorX * wizard.speed * dt)
-    wizard.y = wizard.y + (vectorY * wizard.speed * dt)
+    wizard.collider:setLinearVelocity(vectorX * wizard.speed, vectorY * wizard.speed)
+
+    wizard.x = wizard.collider:getX()
+    wizard.y = wizard.collider:getY() + wizard.height * 0.2 -- adding offset here
+
+    world:update(dt)
 
     wizard.currentAnimation:update(dt)
+
+    cam1:lookAt(wizard.x, wizard.y)
 end
 
 
 function love.draw() 
     cam1:attach()
-    gameMap:drawLayer(gameMap.layers['base floor'])
-    wizard.currentAnimation:draw(wizard.spriteSheet, wizard.x, wizard.y, nil, wizard.scale, wizard.scale)
+    gameMap:drawLayer(
+        gameMap.layers['base floor']
+    )
+    gameMap:drawLayer(
+        gameMap.layers['walls and misc boxes']
+    )
+
+    wizard.currentAnimation:draw(
+        wizard.spriteSheet,
+        wizard.x, wizard.y,
+        nil, 
+        wizard.scale,
+        wizard.scale,
+        wizard.width / 2,
+        wizard.height / 2
+    )
+    -- world:draw() -- uncomment to view collider outlines
+
     cam1:detach()
 end
